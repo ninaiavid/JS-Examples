cf7be83ad5f9195e8c1a5213962c260bf4917a1b
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2021-11-18 19:39:10 +0200

    Add files via upload

---
+++ KelvinWeather.js
@@ -0,0 +1,9 @@
+const kelvin=293; //grade kelvin
+
+const celsius=kelvin-273; // grade celsius
+
+let fahrenheit=celsius*(9/5)+32;//grade fahrenheit
+
+fahrenheit=Math.floor(fahrenheit);//grade fahrenheit rotungit
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
