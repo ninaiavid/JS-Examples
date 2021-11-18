e010c31d323f87248827d855cd705ae1cf092d12
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2021-11-18 20:56:47 +0200

    Update while.js

--- while.js
+++ while.js
@@ -1,3 +1,12 @@
+let n = 1
+while (n <=10 ) {
+  console.log( n )
+  n++
+}
+// Acest cod va afisa numerele de la 1 la 10
+
+
+// Urmatorul cod v-a efectua actiunea inversa
 let n = 10;
 while (n >=1 ) {
   console.log( n );
