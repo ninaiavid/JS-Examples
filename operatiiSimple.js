64dffd0d2de2b8235d6eaca133f7ca7e949362e5
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2021-11-22 16:16:16 +0200

    Add files via upload

---
+++ operatiiSimple.js
@@ -0,0 +1,9 @@
+a = prompt("Enter \"A\" value");
+b = prompt("Enter \"B\" value");
+
+alert("######## VALUES ####### \n a =" + a + "\n b=" + b + 
+"\n ###### OPERATIONS ##### \n a * b = " + (a*b) +            
+"\n a / b =" + (a/b) +           
+"\n a + b =" + (Number(a) + Number(b)) +           
+"\n a - b =" + (a-b) +            
+"\n #######################");
---
+++ operatiiSimple2.js
@@ -0,0 +1,10 @@
+a = prompt("Enter \"A\" value");
+b = prompt("Enter \"B\" value");
+document.write("######## VALUES ####### <br> a = " + a + 
+"<br> b = " + b + 
+"<br> ###### OPERATIONS ##### <br> a * b = " + (a*b) +            
+"<br> a / b = " + (a/b) +           
+"<br> a + b = " + (Number(a) + Number(b)) +           
+"<br> a - b = " + (a-b) +            
+"<br> #######################");
+
