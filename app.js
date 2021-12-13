9c42bf8e98a904ea4803164b67718f146930fe51
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2021-12-13 15:38:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ function kelvinToFahrenheit(kelvin){
     let celsius=kelvin-273; // grade celsius
     fahrenheit=celsius*(9/5)+32;//grade fahrenheit
     fahrenheit=Math.floor(fahrenheit);//grade fahrenheit rotungit
-    console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+    return fahrenheit;
 }
 kelvinToFahrenheit(296);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 //2
+let myAge=38;
+let myAgeInDogYears;
 function humanAgeInDogYears (myAge){
 let earlyYears=2;
 earlyYears*=10.5;//number of dog years accounted for my early years
 laterYears=myAge-2;//later years
 laterYears*=4;//number of dog years accounted for my later years
-let myAgeInDogYears=earlyYears+laterYears;//this will be my age in dog years
+myAgeInDogYears=earlyYears+laterYears;//this will be my age in dog years
+return myAge, myAgeInDogYears;
+} 
+humanAgeInDogYears(36);
 console.log(`I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
-}
-humanAgeInDogYears(38);
-
 //3
 function answerMyQuestion(userName, userQuestion){
 let answer = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes'];
@@ -26,23 +29,29 @@ userName ? console.log (`Hello, ${userName}!`)
 console.log(`${userName} asked: ${userQuestion}`);
 randomNumber=Math.floor(Math.random() * 8);
 eightBall=answer[randomNumber];
-console.log(`The eight ball answered: ${eightBall}`);
+return eightBall;
 }
 answerMyQuestion('nina', 'question?');
-
+console.log(`The eight ball answered: ${eightBall}`);
 //4
+let raceNumber;
+let time='';
 function raceTime(userAge, registeredEarly){
-let raceNumber = Math.floor(Math.random() * 1000);
+raceNumber = Math.floor(Math.random() * 1000);
 if (userAge>18 && registeredEarly) {
     raceNumber+=1000};
 if (userAge>18 && registeredEarly) {
-  console.log(`nr. ${raceNumber} will race at 9.30 am`);
+  time='9.30 am';
+  return time;
 } else if (userAge>18 && !registeredEarly) {
-  console.log(`nr. ${raceNumber} will race at 11.00am`);
+  time='11.00 am';
+  return time;
 } else if (userAge<18) {
-  console.log(`nr.${raceNumber} will race at 12.30pm`);
+  time='12.30 pm'
 } else {
-  console.log('see the registration desk');
+  time=': see the registration desk';
+  return time;
 }
 }
-raceTime(21, true)
+raceTime(20, true);
+console.log(`nr. ${raceNumber} will race at ${time}`);
