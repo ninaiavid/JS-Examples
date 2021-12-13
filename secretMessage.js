ce0e4d8a77ba5954e5ce2e2c2da59b1d97562247
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2021-12-13 21:01:59 +0200

    Add files via upload

---
+++ secretMessage.js
@@ -0,0 +1,19 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+let lastElement=secretMessage.pop();
+
+let newElement=secretMessage.push('to', 'program');
+
+let replace=secretMessage.indexOf('easily');
+secretMessage[replace]='right';
+
+let remove=secretMessage.shift();
+
+let add=secretMessage.unshift('Programming');
+
+replace=secretMessage.indexOf('get');
+secretMessage[replace]='know';
+let removedItems=secretMessage.splice((replace+1), 4)
+
+let message=secretMessage.join();
+console.log(message);
