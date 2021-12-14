50f2cc7f5b63515eea635cf7492f64ede172b0b4
ninaiavid <92196867+ninaiavid@users.noreply.github.com>
2021-12-14 15:00:49 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,18 +9,17 @@ kelvinToFahrenheit(296);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 //2
-let myAge=38;
-let myAgeInDogYears;
 function humanAgeInDogYears (myAge){
 let earlyYears=2;
 earlyYears*=10.5;//number of dog years accounted for my early years
-laterYears=myAge-2;//later years
+let laterYears=myAge-2;//later years
 laterYears*=4;//number of dog years accounted for my later years
 myAgeInDogYears=earlyYears+laterYears;//this will be my age in dog years
 return myAgeInDogYears;
 } 
 humanAgeInDogYears(36);
 console.log(`I am  ${myAgeInDogYears} years old in dog years.`);
+
 //3
 function answerMyQuestion(userName, userQuestion){
 let answer = ['It is certain', 'It is decidedly so', 'Reply hazy try again', 'Cannot predict now', 'Do not count on it', 'My sources say no', 'Outlook not so good', 'Signs point to yes'];
@@ -33,11 +32,10 @@ return eightBall;
 }
 answerMyQuestion('nina', 'question?');
 console.log(`The eight ball answered: ${eightBall}`);
+
 //4
-let raceNumber;
-let time='';
 function raceTime(userAge, registeredEarly){
-raceNumber = Math.floor(Math.random() * 1000);
+let raceNumber = Math.floor(Math.random() * 1000);
 if (userAge>18 && registeredEarly) {
     raceNumber+=1000};
 if (userAge>18 && registeredEarly) {
@@ -45,11 +43,11 @@ if (userAge>18 && registeredEarly) {
 } else if (userAge>18 && !registeredEarly) {
   time='11.00 am';
 } else if (userAge<18) {
-  time='12.30 pm'
+  time='12.30 pm';
 } else {
   time=': see the registration desk';
 }
 return time;
 }
 raceTime(20, false);
-console.log(`nr. ${raceNumber} will race at ${time}`);
+console.log(`you will race at ${time}`);
